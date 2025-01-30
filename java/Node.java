6632bed9aa7de518f8dd026164ca3dee70751a0d
codey19 <cqsheng19@gmail.com>
2025-01-30 14:07:06 -0500

    node class

---
+++ java/Node.java
@@ -0,0 +1,26 @@
+public class Node{
+    private String name;
+    private int weight;
+    public Node(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+
+}
